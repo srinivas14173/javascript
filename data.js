3481c9094a4ec7dd645b27fdb4940a6839c05509
srinivas <s.godhala@farmersinsurance.com>
2022-01-14 20:28:40 -0800

    data types example

---
+++ data.js
@@ -0,0 +1,26 @@
+let num=1;
+let name="tom";
+const person = ["raj", "tom", "kevin"];
+const personddetails = {firstName:"tom", lastName:"wilson", age:50, eyeColor:"black"};
+//boolean data
+    let a=10+"tom";
+    console.log(a);
+    let b="tom"+"wilson";
+    console.log(b);
+    let c="tom"+"10";
+    console.log(c);
+    let d=10+20+"tom";
+    console.log(d);
+    let e="tom"+10+20;
+    console.log(e);
+    let f=10+"tom"+10-1;
+    console.log(f);
+    let g=10+"10"+10-1;
+    console.log(g);
+    let h="10"+10-1;
+    console.log(h);
+
+
+
+
+
