7ca27a5bb744f30964eb859d710beffaab142266
srinivas <s.godhala@farmersinsurance.com>
2022-01-14 19:26:27 -0800

    print data

---
+++ myscript.js
@@ -0,0 +1,16 @@
+function first(){
+    document.getElementById("elementA").innerHTML="ElementB";
+}
+function second(){
+document.write("HeadingB");
+}
+
+function third(){
+alert("third option");
+}
+function fourth(){
+console.log("fourth option");
+}
+
+
+
