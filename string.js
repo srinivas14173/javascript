7b0dadc6e1c7d9d07a8258f84a7070a5bb166b1a
srinivas <s.godhala@farmersinsurance.com>
2022-01-15 17:49:15 -0800

    data string examples

---
+++ string.js
@@ -0,0 +1,20 @@
+let firstName='tom';
+let lastName="wilson";
+let quote="It's my favorite quote";
+let address='2730 First "Street"';
+let state="California";
+    console.log(state.length);
+// Escape Character
+let str1=" It\'s my quote";
+    console.log(str1);
+let str2=" It\"s my quote";
+    console.log(str2);
+let str3=" It\\s my quote";
+    console.log(str3);
+    /*
+        \b backspace
+        \n new line
+        \t  horizontal tab
+        \v  vertical tab
+      */
+
